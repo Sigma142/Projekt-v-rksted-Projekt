3e351c419e55de2d529f0b43720730309b924f00
Sigma142 <Christian.lp06@gmail.com>
2025-11-13 09:06:37 +0100

    spil

---
+++ Spill projekt/Assets/This is how we move (NIGGA).cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class Thisishowwemove : MonoBehaviour
+{
+    Vector3 velocity;
+    float gravity = -9.81f;
+
+    public float moveSpeed = 5f;
+    public Transform cameraTransform;
+
+    private CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 forward = cameraTransform.forward;
+        Vector3 right = cameraTransform.right;
+
+        forward.y = 0f;
+        right.y = 0f;
+        forward.Normalize();
+        right.Normalize();
+
+        Vector3 moveDirection = forward * vertical + right * horizontal;
+
+        controller.Move(moveDirection * moveSpeed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+    }
+
+}
