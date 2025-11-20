46c28938b4db8839161df95cd4e15437a9bf6699
CanadianEagle11 <whcp2020@gmail.com>
2025-11-20 09:54:05 +0100

    fixer lowky over 999+ besked i frame

--- Spill projekt/Assets/pille pickup.cs
+++ Spill projekt/Assets/pille pickup.cs
@@ -15,11 +15,9 @@ public class pillepickup : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(transform.position);
         RaycastHit hit;
         if (Physics.Raycast(transform.position, transform.forward, out hit, lunge, mask))
         {
-           Debug.Log(hit.collider.name);
             if (hit.collider.CompareTag("Pills"))
             {
                 text.text = "Tryk E for at samle pille op";
@@ -31,13 +29,11 @@ public class pillepickup : MonoBehaviour
             }
             else
             {
-                Debug.Log("ikke piller");
                 text.text = "";
             }
         }
         else
         {
-            Debug.Log("ingen hit");
             text.text = "";
         }
     }
